c15b172632e9e4f7047e1134dcd0043099f567ad
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-07-14 08:18:57 -0300

    Validação usando a lib flunt com abordagem de Design By Contracts

--- PaymentContext.Domain/ValueObjects/Document.cs
+++ PaymentContext.Domain/ValueObjects/Document.cs
@@ -1,3 +1,4 @@
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
 using PaymentContext.Shared.ValueObjects;
 
@@ -9,9 +10,27 @@ namespace PaymentContext.Domain.ValueObjects
         {
             Number = number;
             Type = type;
+
+            AddNotifications(new Contract()
+                .Requires()
+                .IsTrue(Validate(), "Document.Number", "Documento Inválido")
+            );
         }
 
+        private const int CNPJ_LENGHT = 14;
+        private const int CPF_LENGHT = 11;
         public string Number { get; private set; }
         public EDocumentType Type { get; private set; }
+        
+        private bool Validate() 
+        {
+            if (Type == EDocumentType.CNPJ && Number.Length == CNPJ_LENGHT)
+                return true;
+            
+            if (Type == EDocumentType.CPF && Number.Length == CPF_LENGHT)
+                return true;
+
+            return false;
+        }
     }
 }
--- PaymentContext.Domain/ValueObjects/Email.cs
+++ PaymentContext.Domain/ValueObjects/Email.cs
@@ -1,3 +1,4 @@
+using Flunt.Validations;
 using PaymentContext.Shared.ValueObjects;
 
 namespace PaymentContext.Domain.ValueObjects
@@ -8,6 +9,11 @@ namespace PaymentContext.Domain.ValueObjects
         public Email(string address)
         {
             Address = address;
+
+            AddNotifications(new Contract()
+                .Requires()
+                .IsEmail(Address, "Email.Address", "E-mail inválido")
+            );
         }
 
         public string Address { get; private set; }
